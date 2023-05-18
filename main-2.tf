c4aa637bced2f50ddc8d918813b33aa7ed71ad2a
emnwankwo <87596722+emnwankwo@users.noreply.github.com>
2023-05-18 03:30:27 -0500

    Create main-2.tf
    Create main-2.tf for testing drift detection

---
+++ main-2.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region     = "us-west-2"
+}
+
+resource "aws_security_group" "Ec2_security1" {
+  name        = "TerraformEc2_security1"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = vpc-03253122f3b3dad51"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+}
